5002e113e8002316df5ccb4de90089ea2d302e51
Thanh Minh <minhuit1991@gmail.com>
2017-08-29 17:14:21 +0700

    Initial

---
+++ WebSocketManager/Common/InvocationDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSocketManager.Common
+{
+    public class InvocationDescriptor
+    {
+        [JsonProperty("methodName")]
+        public string MethodName { get; set; }
+
+        [JsonPropertyAttribute("arguments")]
+        public object[] Arguments { get; set; }
+    }
+}
---
+++ WebSocketManager/Common/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSocketManager.Common
+{
+    public enum MessageType
+    {
+        Text,
+        ClientMethodInvocation,
+        ConnectionEvent
+    }
+
+    public class Message
+    {
+        public MessageType MessageType { get; set; }
+        public string Data { get; set; }
+    }
+}
